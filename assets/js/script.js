00d7f5331e7bafe06bed4ed28ad9dd7e08ae8719
Erica <ericamisiorski@gmail.com>
2021-11-02 20:30:11 -0400

    update html with map

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+
+
+if (window.addEventListener) { window.addEventListener("message", function (event) { if (event.data.length >= 22) { if (event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); } }, false); } else if (window.attachEvent) { window.attachEvent("message", function (event) { if (event.data.length >= 22) { if (event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); } }, false); }
