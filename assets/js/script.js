adf8099ae8840220b15a1d4e85a9d14e9f2b5d43
Adam Calcasola <adam.calcasola@gmail.com>
2021-11-04 19:38:30 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-var apikey = "57bf365637e080dcba9bad64d8d27cd9";
-var openSecretsURL = "https://www.opensecrets.org/api/?method=candIndByInd&cid=N00007360&cycle=2020&ind=K02&apikey="
+var apiKey = "&apikey=57bf365637e080dcba9bad64d8d27cd9";
+var openSecretsUrl = "http://www.opensecrets.org/api/?method=getLegislators&output=json&id=";
+
+var stateSelect = document.querySelector("#state");
+
+fetch(openSecretsUrl + "CT" + apiKey)
+    .then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+    })
 
 
-if (window.addEventListener) { window.addEventListener("message", function (event) { if (event.data.length >= 22) { if (event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); } }, false); } else if (window.attachEvent) { window.attachEvent("message", function (event) { if (event.data.length >= 22) { if (event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); } }, false); }
+
+stateSelect.addEventListener('change', (event) => {
+    console.log(stateSelect.value);
+});
+
+
+//if (window.addEventListener) { window.addEventListener("message", function (event) { if (event.data.length >= 22) { if (event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); } }, false); } else if (window.attachEvent) { window.attachEvent("message", function (event) { if (event.data.length >= 22) { if (event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); } }, false); }
 
